d8fe08235314da4943a9c382d5915ec05f35d3dd
Kristina Kolibab <tinytina228@yahoo.com>
2017-04-20 19:27:24 -0400

    Jabber almost done!

--- jabber.c
+++ jabber.c
@@ -4,17 +4,20 @@
 #include <string.h>
 #include <limits.h>
 #include <math.h>
+#include <stdbool.h>
 
 int bit_len = 0; // keep track of the bit-length
 int index_count = 0; // keep track of overall index
+int bit_counter = 0; // debug method to make sure I'm getting throught the entire file, 
+// wherever you read in another bit, increment this and print it out
 
 // creating my own power function because the STL uses double's
-int Pow(int x, int n){
+int Pow(int base, int exp){
 	
 	int number = 1;
 	
-	for(int i = 0; i < n; ++i){
-		number *= x;
+	for(int i = 0; i < exp; ++i){
+		number *= base;
 	}
 	return number;
 }
@@ -26,61 +29,103 @@ struct b_tree {
 	struct b_tree * right, * left;
 };
 
-typedef struct b_tree node; // use this to create other node objects
+typedef struct b_tree node; // node object
 
 // insert puts a new node into the tree, if no tree one will be created
-void insert(node * head, int bit){
+void insert(node * head, char bit, FILE * inputFile){
 
-	// increment bit_length as 2^bit_length 
-        if(index_count > Pow(2, bit_len)){
-                ++bit_len;
-        }
-
-	// bit_len of 0 obviously means it's the first bit (no pattern)
-	if(bit_len == 0){
+	//first case
+	if(index_count == 0){
 		node * temp_node = NULL;
 		temp_node = (node *)malloc(sizeof(node));
 		temp_node->left = temp_node->right = NULL;
 		temp_node->index = index_count;
 		++index_count; // always increment index
 		head = temp_node;
-		printf("Pattern at index %d: 0\n", head->index); 
+
+		if(bit == '1'){
+			node * new_node = NULL;
+			new_node = (node *)malloc(sizeof(node));
+			new_node->left = new_node->right = NULL;
+			new_node->index = index_count;
+			++index_count;	
+			head->left = new_node;
+		} else if(bit == '0'){	
+			node * new_node = NULL;
+			new_node = (node *)malloc(sizeof(node));
+			new_node->left = new_node->right = NULL;
+			new_node->index = index_count;
+			++index_count;
+			head->left = new_node;
+		}
+			 
 	}
 	// have a while loop here that terminates at largest known pattern
-	else if(bit == 1){ 
-		if(head->right != NULL){ 	
-			head = head->right;
-			head->index = index_count;
-			++index_count;
-		} else if(head->right == NULL){
-			node * new_node = head->right;
-			new_node->index = index_count;
-			++index_count;
+	while((bit = fgetc(inputFile)) != EOF){			
+		if(bit == '\n') break; // without this the while loop doesn't terminate
+	
+		printf("bit %i: %c\n", bit_counter, bit);
+	        ++bit_counter;
+
+		// increment bit_length as 2^bit_length
+		if(index_count > Pow(2, bit_len)){
+			++bit_len;
 		}
-	} else if(bit == 0){
-		if(head->left != NULL){ 
-			head = head->left;
-			head->index = index_count;
-			++index_count;
-		} else if(head->left == NULL){
-			node * new_node = head->left;
-			new_node->index = index_count;
-			++index_count;
+
+		bool done = false;
+		while(done == false){ //each new pattern
+			if(bit == '1'){ 
+				if(head->right){ 
+					head = head->right;
+					bit = fgetc(inputFile);
+	
+					printf("bit %i: %c\n", bit_counter, bit);
+				        ++bit_counter;
+
+				} else if(!(head->right)){ // if pattern ends, set the bit
+					node * new_node = NULL;
+					new_node = (node *)malloc(sizeof(node));		
+					new_node->left = new_node->right = NULL;
+					new_node->index = index_count;
+					++index_count;
+					head->right = new_node;
+					done = true;
+				}
+			} else if(bit == '0'){
+				if(head->left){ 
+					head = head->left;
+					bit = fgetc(inputFile);
+
+					printf("bit %i: %c\n", bit_counter, bit);
+				        ++bit_counter;
+
+				} else if(!(head->left)){ // if pattern ends, set the bit
+					node * new_node = NULL;	
+					new_node = (node *)malloc(sizeof(node));
+					new_node->left = new_node->right = NULL;
+					new_node->index = index_count;
+					++index_count;
+					head->left = new_node;
+					done = true;
+				}
+			}
 		}
 	}
+	printf("EOF\n");
 }
-
-// should we make a print function? or print as we add ^^?
 
 // jabber reads in bits from file then puts it into the binary tree/table
 void jabber( FILE * inputFile, FILE * outputFile ){
 	
 	node * top_node;
-	int bit = 0;
-	
-	while( (bit = fgetc(inputFile)) != EOF){
-		insert(top_node, bit);
-	}
+	char bit;
+	bit = fgetc(inputFile);
+	printf("bit %i: %c\n", bit_counter, bit);
+	++bit_counter;
+	insert(top_node, bit, inputFile);
+
+	// we might think about combining the jabber and insert function, this doesn't 
+	// really do anything too crazy that can't quickly be added to insert
 }
 
 
